3f54f1b3f68004583d6b4e1466945383c9f29dbb
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-28 09:36:24 -0700

    string

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 def main():
     numbers = []
+    Fvalue = 0
+    while True: 
+        Value = int(input("Input number"))
+        if Fvalue is 0 or Value < Fvalue:
+            numbers.append(Value)
+            Fvalue = Value
+        else:
+            break
+       
+   
+   
+   
+   
     """
     ########################################
     Code Your Program here
